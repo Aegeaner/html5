95f90b72404f6f23e2212b5d0a64cc13900a8128
Aegeaner <xihuke@gmail.com>
2013-06-23 15:10:09 +0800

    区分相交的线并加粗

--- Untangle/js/html5games.untangle.js
+++ Untangle/js/html5games.untangle.js
@@ -13,6 +13,7 @@ function Line(startPoint, endPoint, thickness) {
 var untangleGame = {
 	circles: [],
 	thinLinethickness: 1,
+	boldLinethickness: 5,
 	lines: []
 };
 
@@ -49,6 +50,7 @@ function connectCircles() {
 			untangleGame.lines.push(new Line(startPoint, endPoint, untangleGame.thinLinethickness));
 		}
 	}
+	updateLineIntersection();
 }
 
 $(function() {
@@ -124,5 +126,65 @@ function gameloop() {
 	for(var i = 0; i < untangleGame.circles.length; i++) {
 		var circle = untangleGame.circles[i];
 		drawCircle(ctx, circle.x, circle.y, circle.radius);
+	}
+}
+
+// 检测给定的两条线是否相交
+function isIntersect(line1, line2)
+{
+	// 转换成一般式： Ax + By = C
+	var a1 = line1.endPoint.y - line1.startPoint.y;
+	var b1 = line1.startPoint.x - line1.endPoint.x;
+	var c1 = a1 * line1.startPoint.x + b1 * line1.startPoint.y;
+
+	var a2 = line2.endPoint.y - line2.startPoint.y;
+	var b2 = line2.startPoint.x - line2.endPoint.x;
+	var c2 = a2 * line2.startPoint.x + b2 * line2.startPoint.y;
+
+	// 计算交点
+	var d = a1 * b2 - a2 * b1;
+
+	// 当 d 等于 0 时， 两线平行
+	if(d == 0) {
+		return false;
+	} else {
+		var x = (b2 * c1 - b1 * c2) / d;
+		var y = (a1 * c2 - a2 * c1) / d;
+
+		// 检测截点是否在两条线段之上
+		if((isInBetween(line1.startPoint.x, x, line1.endPoint.x) ||
+			isInBetween(line1.startPoint.y, y, line1.endPoint.y)) &&
+			(isInBetween(line2.startPoint.x, x, line2.endPoint.x) ||
+			isInBetween(line2.startPoint.y, y, line2.endPoint.y)))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+// 如果b在a与c之间返回true
+function isInBetween(a, b, c) {
+	// 如果b几乎等于a或c，返回false
+	if(Math.abs(a-b) < 0.000001 || Math.abs(b-c) < 0.000001) {
+		return false;
+	}
+
+	return (a < b && b < c) || (c < b && b < a);
+}
+
+// 检测相交的线并加粗
+function updateLineIntersection()
+{
+	for(var i = 0; i < untangleGame.lines.length; i++) {
+		for(var j = 0; j < i; j++) {
+			var line1 = untangleGame.lines[i];
+			var line2 = untangleGame.lines[j];
+
+			if(isIntersect(line1, line2)) {
+				line1.thickness = untangleGame.boldLinethickness;
+				line2.thickness = untangleGame.boldLinethickness;
+			}
+		}
 	}
 }
