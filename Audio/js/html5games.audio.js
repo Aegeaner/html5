6b7a7841325efaebe5fe140999bb8cf014777a68
Aegeaner <xihuke@gmail.com>
2013-06-23 17:52:39 +0800

    给音乐游戏创建一个基本的背景

--- Audio/js/html5games.audio.js
+++ Audio/js/html5games.audio.js
@@ -19,4 +19,41 @@ $(function() {
 
 		return false;
 	});
+
+	drawBackground();
 });
+
+function drawBackground()
+{
+	var game = document.getElementById("game-background-canvas");
+	var ctx = game.getContext("2d");
+
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = "#000";
+
+	var center = game.width / 2;
+
+	// 绘制三条竖线
+	ctx.beginPath();
+	ctx.moveTo(center-100, 50);
+	ctx.lineTo(center-100, game.height-50);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(center, 50);
+	ctx.lineTo(center, game.height-50);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(center+100, 50);
+	ctx.lineTo(center+100, game.height-50);
+	ctx.stroke();
+
+	// 绘制横线
+	ctx.beginPath();
+	ctx.moveTo(center-150, game.height-80);
+	ctx.lineTo(center+150, game.height-80);
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = "rgba(50, 50, 50, .8)";
+	ctx.stroke();
+}
