a71b4df5ddfb9b30512be0319c112a8696059a5e
Aegeaner <xihuke@gmail.com>
2013-06-23 13:43:13 +0800

    将画圆功能封装成函数

--- Untangle/js/html5games.untangle.js
+++ Untangle/js/html5games.untangle.js
@@ -1,41 +1,26 @@
+var untangleGame = {};
+
+function drawCircle(ctx, x, y, radius) {
+	ctx.fillStyle = "rgba(200, 200, 100, .9)";
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.fill();
+}
+
 $(function() {
 	var canvas = document.getElementById("game");
 	var ctx = canvas.getContext("2d");
-	ctx.fillStyle = "rgba(200, 200, 100, .6)";
 	
-	// 绘制下半圆
-	ctx.beginPath();
-	ctx.arc(100, 110, 50, 0, Math.PI);
-	ctx.closePath();
-	ctx.fill();
+	var circleRadius = 10;
 
-	// 绘制上半圆
-	ctx.beginPath();
-	ctx.arc(100, 90, 50, 0, Math.PI, true);
-	ctx.closePath();
-	ctx.fill();
+	var width = canvas.width;
+	var height = canvas.height;
 
-	// 绘制左半圆
-	ctx.beginPath();
-	ctx.arc(230, 100, 50, Math.PI/2, Math.PI*3/2);
-	ctx.closePath();
-	ctx.fill();
-
-	// 绘制右半圆
-	ctx.beginPath();
-	ctx.arc(250, 100, 50, Math.PI*3/2, Math.PI/2);
-	ctx.closePath();
-	ctx.fill();
-
-	// 绘制一个接近圆的形状
-	ctx.beginPath();
-	ctx.arc(180, 240, 50, Math.PI*7/6, Math.PI*2/3);
-	ctx.closePath();
-	ctx.fill();
-
-	// 绘制一小段圆弧
-	ctx.beginPath();
-	ctx.arc(150, 250, 50, Math.PI*7/6, Math.PI*2/3, true);
-	ctx.closePath();
-	ctx.fill();
+	var circleCount = 10;
+	for(var i = 0; i < circleCount; i++) {
+		var x = Math.random() * width;
+		var y = Math.random() * height;
+		drawCircle(ctx, x, y, circleRadius);
+	}
 });
